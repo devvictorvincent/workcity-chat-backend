da1fb871fd2c9e29e2ccf6021d37941318715526
Victor Vincent <dev.victorvincent@gmail.com>
2025-08-15 17:51:43 +0100

    setting up project structure

---
+++ server.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+ 
+app.use(cors());
+app.use(express.json());
+
+ 
+app.get('/', (req, res) => {
+  res.send('Chat Backend is running');
+});
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB error:', err));
+
+ 
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+
+io.on('connection', (socket) => {
+  console.log('🔌 User connected:', socket.id);
+
+  socket.on('send_message', (data) => {
+    console.log('Message received:', data);
+     
+    io.emit('receive_message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
