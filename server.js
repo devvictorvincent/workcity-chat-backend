7758a2a6f80cf4ad204b46bf64f8ae723792f3e0
Victor Vincent <dev.victorvincent@gmail.com>
2025-08-16 04:50:50 +0100

    add socket integration

--- server.js
+++ server.js
@@ -5,40 +5,85 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const { Server } = require('socket.io');
 const authRoutes = require('./routes/auth');
+const connectDB = require('./config/db');
+const conversationRoutes = require('./routes/conversations');
+const messageRoutes = require('./routes/messages');
+const Message = require('./models/Message');
 
 const app = express();
 const server = http.createServer(app);
 
- 
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
- 
 app.get('/', (req, res) => {
   res.send('Chat Backend is running');
 });
 
 app.use('/auth', authRoutes);
+app.use('/conversations', conversationRoutes);
+app.use('/messages', messageRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB error:', err));
-
- 
 const io = new Server(server, {
   cors: { origin: '*' }
 });
 
+const activeUsers = new Map();
+
 io.on('connection', (socket) => {
   console.log('🔌 User connected:', socket.id);
 
-  socket.on('send_message', (data) => {
-    console.log('Message received:', data);
-     
-    io.emit('receive_message', data);
+  socket.on('user_join', (userId) => {
+    activeUsers.set(userId, socket.id);
+    socket.userId = userId;
+    console.log(`User ${userId} joined`);
+  });
+
+  socket.on('join_conversation', (conversationId) => {
+    socket.join(conversationId);
+    console.log(`User joined conversation: ${conversationId}`);
+  });
+
+  socket.on('send_message', async (data) => {
+    try {
+      const { conversationId, text, senderId } = data;
+      
+      const message = await Message.create({
+        conversationId,
+        senderId,
+        text,
+        readBy: [senderId]
+      });
+      
+      await message.populate('senderId', 'name email role');
+      
+      io.to(conversationId).emit('receive_message', message);
+    } catch (error) {
+      console.error('Error sending message:', error);
+      socket.emit('message_error', { error: error.message });
+    }
+  });
+
+  socket.on('typing_start', (data) => {
+    socket.to(data.conversationId).emit('user_typing', {
+      userId: socket.userId,
+      conversationId: data.conversationId
+    });
+  });
+
+  socket.on('typing_stop', (data) => {
+    socket.to(data.conversationId).emit('user_stop_typing', {
+      userId: socket.userId,
+      conversationId: data.conversationId
+    });
   });
 
   socket.on('disconnect', () => {
+    if (socket.userId) {
+      activeUsers.delete(socket.userId);
+    }
     console.log('User disconnected:', socket.id);
   });
 });
