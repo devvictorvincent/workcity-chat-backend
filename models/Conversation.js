e9349e0ea786e2a1e9a7ab5f7672ed2a5af402c7
Victor Vincent <dev.victorvincent@gmail.com>
2025-08-16 03:55:17 +0100

    update Models

--- models/Conversation.js
+++ models/Conversation.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
 const conversationSchema = new mongoose.Schema({
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  participants: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User',
+    required: true 
+  }],
+  lastMessage: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Message' 
+  },
+  isGroup: { type: Boolean, default: false },
+  groupName: String,
+  groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Conversation', conversationSchema);
--- models/Message.js
+++ models/Message.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-  conversationId: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation' },
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  text: String,
-  readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  conversationId: { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation', required: true },
+  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  text: { type: String, required: true },
+  messageType: { type: String, enum: ['text', 'image', 'file'], default: 'text' },
+  fileUrl: String,
+  readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  isDeleted: { type: Boolean, default: false }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Message', messageSchema);
