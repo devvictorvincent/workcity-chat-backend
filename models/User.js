1d4968a830a1bf41edd40fca67b96f9306db56a6
Victor Vincent <dev.victorvincent@gmail.com>
2025-08-16 05:08:29 +0100

    update user model to support profile updates

--- models/User.js
+++ models/User.js
@@ -6,16 +6,33 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['admin', 'agent', 'customer', 'designer', 'merchant'], default: 'customer' },
+  profilePhoto: { type: String, default: '' },
+  bio: { type: String, default: '' },
+  phone: { type: String, default: '' },
+  address: {
+    street: { type: String, default: '' },
+    city: { type: String, default: '' },
+    state: { type: String, default: '' },
+    zipCode: { type: String, default: '' },
+    country: { type: String, default: '' }
+  },
+  isActive: { type: Boolean, default: true },
+  lastSeen: { type: Date, default: Date.now },
+  preferences: {
+    notifications: { type: Boolean, default: true },
+    emailNotifications: { type: Boolean, default: true },
+    darkMode: { type: Boolean, default: false }
+  }
 }, { timestamps: true });
 
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 12);
   next();
 });
 
-userSchema.methods.comparePassword = function(pw) {
-  return bcrypt.compare(pw, this.password);
+userSchema.methods.comparePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
